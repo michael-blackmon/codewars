96096573de367bbec906a254e25ae7debe7d7306
Michael Blackmon <michaeljblackmon@yahoo.com>
2023-09-05 08:12:12 -0700

    anotha one

---
+++ C++/8kyu/code_under_pressure_1.cpp
@@ -0,0 +1,12 @@
+/*
+8kyu
+INSTRUCTIONS:
+DESCRIPTION:
+Code as fast as you can! You need to double the integer and return it.
+*/
+#include <cstdint>
+
+int32_t double_integer(int32_t n)
+{
+    return n * 2;
+}
