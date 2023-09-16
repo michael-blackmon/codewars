e8f7c9614569265eea093b9b9a46a801562edc41
Michael Blackmon <michaeljblackmon@yahoo.com>
2023-09-16 08:12:12 -0700

    anotha one

---
+++ C++/8kyu/returning_strings.cpp
@@ -0,0 +1,14 @@
+/*
+8kyu
+INSTRUCTIONS:
+DESCRIPTION:
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+#include <string>
+
+std::string greet(const std::string &n)
+{
+    return "Hello, " + n + " how are you doing today?";
+}
