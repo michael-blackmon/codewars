2ab632fec03084de50cce4f91ca6ccd7920c53a6
Michael Blackmon <michaeljblackmon@yahoo.com>
2023-09-17 08:12:12 -0700

    anotha one

---
+++ C++/8kyu/get_char_from_ascii.cpp
@@ -0,0 +1,17 @@
+/*
+8kyu
+INSTRUCTIONS:
+DESCRIPTION:
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+For ASCII table, you can refer to http://www.asciitable.com/
+*/
+char get_char(int i)
+{
+    return char(i);
+}
