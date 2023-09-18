5907bb7f3db0b1afe8a5a62f25547ee7c06645ed
Michael Blackmon <michaeljblackmon@yahoo.com>
2023-09-18 08:12:12 -0700

    anotha one

---
+++ C++/8kyu/grasshopper_personalized_message.cpp
@@ -0,0 +1,18 @@
+/*
+8kyu
+INSTRUCTIONS:
+DESCRIPTION:
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+*/
+#include <string>
+
+std::string greet(const std::string &name, const std::string &owner)
+{
+    return (name == owner) ? "Hello boss" : "Hello guest";
+}
